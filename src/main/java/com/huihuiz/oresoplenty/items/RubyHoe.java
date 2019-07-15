917a21d295197d8122cc441814bb6a44c92c6ac3
zwang3583 <zwan1312@bu.edu>
2019-07-14 22:16:13 -0400

    ruby toolset

--- src/main/java/com/huihuiz/oresoplenty/items/RubyHoe.java
+++ src/main/java/com/huihuiz/oresoplenty/items/RubyHoe.java
@@ -1,4 +1,4 @@
 package com.huihuiz.oresoplenty.items;
 
-public class RubySword {
+public class RubyHoe {
 }
--- src/main/java/com/huihuiz/oresoplenty/items/RubyPickaxe.java
+++ src/main/java/com/huihuiz/oresoplenty/items/RubyPickaxe.java
@@ -1,16 +1,4 @@
 package com.huihuiz.oresoplenty.items;
 
-import com.huihuiz.oresoplenty.OresOPlenty;
-import net.minecraft.item.Item;
-import net.minecraftforge.common.ToolType;
-
-public class RubyPickaxe extends Item {
-
-    public RubyPickaxe(){
-        super(new Item.Properties()
-                .maxStackSize(1)
-                .addToolType(ToolType.PICKAXE, 3)
-                .group(OresOPlenty.setup.itemGroup));
-        setRegistryName("rubypickaxe");
-    }
+public class RubyPickaxe {
 }
